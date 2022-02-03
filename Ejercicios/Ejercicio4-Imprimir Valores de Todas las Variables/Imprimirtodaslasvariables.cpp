af460e299868307594b69958b4d79e87bf5913b2
kmepa <98782018+kmepa@users.noreply.github.com>
2022-02-02 22:12:56 -0600

    Add files via upload

---
+++ Ejercicios/Ejercicio4-Imprimir Valores de Todas las Variables/Imprimirtodaslasvariables.cpp
@@ -0,0 +1,31 @@
+/*
+    Proposito:
+    Autor :Kelin Mejia
+    Fecha:
+	hora:
+	*/
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Spanish");
+	cout.precision(12);
+bool valorBooleam;
+float valorBoolean= true;
+
+int valorEntero= 15;
+double valorDouble= 20.99;
+string valorString="Hola como esta";
+
+
+cout << "Valor Booleam:" <<valorBoolean<< endl;
+cout << "Valor Entero:" <<valorEntero<< endl;
+cout << "Valor Double:"  <<valorDouble<< endl;
+cout <<"valor String:" <<valorString<< endl;
+
+
+
+	return 0;
+}
---
+++ Ejercicios/Ejercicio4-Imprimir Valores de Todas las Variables/Imprimirvalordetodaslasvariablescpp.cpp
@@ -0,0 +1,14 @@
+/*
+    Proposito: Practicar
+    Autor :Kelin Mejia
+    Fecha:
+    mond
+*/
+
+
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+
